1d3740c7188dc53fd7a3ae277eeea85f2d23259f
Likhith reddy <reddylikith104@gmail.com>
2020-08-05 10:57:39 +0530

    Initial commit

---
+++ sample2.js
@@ -0,0 +1,7 @@
+document.getElementById('clic').addEventListener('click',add);
+function add(){
+var x=parseInt(document.getElementById('n1').value);
+var y=parseInt(document.getElementById('n2').value);
+document.getElementById('result').innerHTML=x+y;
+return false;
+}
